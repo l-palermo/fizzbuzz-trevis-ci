416f21cd23323f5cc84920c23133ec49fd9e0773
Luigi <l-palermo@hotmail.it>
2019-05-27 14:21:11 +0000

    Condition for buzz with rspec

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,5 +1,7 @@
 def fizzbuzz(number)
     if number % 3 == 0
-        return "fizz"
+      return "fizz"
+    elsif number % 5 == 0
+      return "buzz"
     end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,11 @@
 require 'fizzbuzz'
 
 describe 'fizzbuzz' do 
+   
    it 'return "fizz" when passed 3' do
        expect(fizzbuzz(3)).to eq "fizz"
    end
+   
+   it 'return "buzz" when passed 5' do 
+      expect(fizzbuzz(5)).to eq "buzz"
 end
